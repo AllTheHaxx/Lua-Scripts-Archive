39694736f5e4aad46d50ed89dd07729fbd4f194d
Henningstone <Henningstone@users.noreply.github.com>
2017-02-13 00:22:43 +0100

    added debug lib

---
+++ .library/debug.lua
@@ -0,0 +1,49 @@
+--[[#!
+	#debug
+]]--#
+
+
+function locals()
+    local variables = {}
+    local idx = 1
+    while true do
+        local ln, lv = debug.getlocal(2, idx)
+        if ln ~= nil then
+            variables[ln] = lv
+        else
+            break
+        end
+        idx = 1 + idx
+    end
+    print("----- LOCALS -----")
+    for k,v in next, variables do print(k,"=",v) end
+    print("--- END LOCALS ---")
+    return variables
+end
+
+function globals()
+    print("----- GLOBALS -----")
+    for k,v in next, _G do print(k,"=",v) end
+    print("--- END GLOBALS ---")
+    return _G
+end
+
+function upvalues()
+    local variables = {}
+    local idx = 1
+    local func = debug.getinfo(2, "f").func
+    while true do
+        local ln, lv = debug.getupvalue(func, idx)
+        if ln ~= nil then
+            variables[ln] = lv
+        else
+            break
+        end
+        idx = 1 + idx
+    end
+    print("----- UPVALUES -----")
+    for k,v in next, variables do print(k,"=",v) end
+    print("--- END UPVALUES ---")
+
+    return variables
+end
